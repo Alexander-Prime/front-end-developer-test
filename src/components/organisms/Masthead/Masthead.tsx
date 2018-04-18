465fb43cfd49084f0b0b0c5d170318590db99980
Alexander Peters <rainesabaal@gmail.com>
2018-04-18 16:07:26 -0700

    Add "Create post" link"

--- src/components/organisms/Masthead/Masthead.tsx
+++ src/components/organisms/Masthead/Masthead.tsx
@@ -1,5 +1,6 @@
 import classnames from "classnames";
 import React from "react";
+import { Icon } from "react-atoms";
 import { Link } from "react-router-dom";
 
 import { CommonAttributes } from "common/types";
@@ -10,9 +11,14 @@ interface Props extends CommonAttributes {}
 
 const Masthead = (props: Props) => (
   <header className={classnames(props.className, c.masthead)}>
-    <Link className={c["masthead-title"]} to="/">
-      A Big Ol' Blog
-    </Link>
+    <nav className={c["masthead-nav"]}>
+      <Link className={c["masthead-nav-title"]} to="/">
+        A Big Ol' Blog
+      </Link>
+      <Link className={c["masthead-nav-createPost"]} to="/create">
+        <Icon name="create" /> Create post
+      </Link>
+    </nav>
   </header>
 );
 
